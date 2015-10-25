cb8088e64a4fdcea5044812680cc4ebe6bb8a75c
Peter Mosmans <support@go-forward.net>
2015-10-25 14:35:56 +1000

    Implemented TLS record fragmentation
    Added distinction between TLS record types
    Added docstrings
    Refactored some functions

--- tpa.py
+++ tpa.py
@@ -14,14 +14,15 @@ import dpkt
 import pcap
 
 from constants import PRETTY_NAMES
-TLS_HANDSHAKE = 22
+
 global streambuffer
 streambuffer = {}
 
 
 class Extension:
-    """Encapsulates TLS extensions."""
-
+    """
+    Encapsulates TLS extensions.
+    """
     def __init__(self, payload):
         self._type_id, payload = unpacker('H', payload)
         self._type_name = pretty_print_name('extension_type', self._type_id)
@@ -40,6 +41,9 @@ class Extension:
 
 
 def analyze_packet(timestamp, packet):
+    """
+    Main analysis loop for pcap.
+    """
     eth = dpkt.ethernet.Ethernet(packet)
     if isinstance(eth.data, dpkt.ip.IP):
         parse_ip_packet(eth.data)
@@ -97,7 +101,7 @@ def list_interfaces():
     for name in pcap.findalldevs():
         prettydevicename = ''
         queryname = name
-        if name.startswith('\Device\NPF_'):
+        if name.startswith("\Device\NPF_"):
             queryname = name[12:]
         if name.endswith('}'):
             prettydevicename = 'eth{0} '.format(i)
@@ -111,95 +115,121 @@ def list_interfaces():
 
 
 def parse_ip_packet(ip):
+    """
+    Parses IP packet.
+    """
     sys.stdout.flush()
     if (isinstance(ip.data, dpkt.tcp.TCP) and len(ip.data.data)):
         parse_tcp_packet(ip)
 
 
 def parse_tcp_packet(ip):
+    """
+    Parses TCP packet.
+    """
     global streambuffer
     connection = '{0}:{1}-{2}:{3}'.format(socket.inet_ntoa(ip.src),
                                           ip.data.sport,
                                           socket.inet_ntoa(ip.dst),
                                           ip.data.dport)
-    if streambuffer.has_key(connection):
-        # TODO: add pieces in the right order
-        verboseprint('Added sequence number {0:12d} to buffer'.
-                     format(ip.data.seq))
-        stream = streambuffer[connection] + ip.data.data
-        del streambuffer[connection]
-        if len(stream) > (10000):
-            verboseprint('Dumped partial stream, probably broken ({0} bytes)'.
-                         format(len(stream)))
-            return
-    else:
-        if ord(ip.data.data[0]) in set((20, 21, 22)):
+    if ord(ip.data.data[0]) in set((20, 21, 22)):
             stream = ip.data.data
+    else:
+        if streambuffer.has_key(connection):
+            # TODO: add pieces in the right order
+            verboseprint('Added sequence number {0:12d} to buffer'.
+                         format(ip.data.seq))
+            stream = streambuffer[connection] + ip.data.data
+            del streambuffer[connection]
+            if len(stream) > (10000):
+                verboseprint('Flushed buffer ({0} bytes)'.
+                             format(len(stream)))
         else:
             return
-    parse_tls_record(ip, stream)
+    parse_tls_records(ip, stream)
 
 
-def parse_tls_record(ip, stream):
+def add_to_buffer(ip, partial_stream):
+    """
+    Adds partial_stream of ip to global stream buffer.
+    """
     global streambuffer
-    records = []
     connection = '{0}:{1}-{2}:{3}'.format(socket.inet_ntoa(ip.src),
                                           ip.data.sport,
                                           socket.inet_ntoa(ip.dst),
                                           ip.data.dport)
-    try:
-        records, bytes_used = dpkt.ssl.tls_multi_factory(stream)
-    except:
-        verboseprint('exception - issue parsing TLS data')
-        return
+    streambuffer[connection] = partial_stream
+    verboseprint('Added {0} bytes (seq {1}) to streambuffer for {2}'.
+                 format(len(partial_stream), ip.data.seq, connection))
+
+
+def parse_tls_records(ip, stream):
+    """
+    Parses TLS Records.
+    """
+    records, bytes_used = dpkt.ssl.tls_multi_factory(stream)
     if bytes_used != len(stream):
-        streambuffer[connection] = stream[bytes_used:]
-        verboseprint('Added {0} bytes to streambuffer for connection pair {1}'.
-                     format(len(stream[bytes_used:]), connection))
+        add_to_buffer(ip, stream[bytes_used:])
     for record in records:
-        # to do: split based on record type
-        try:
-            handshake = dpkt.ssl.TLSHandshake(record.data)
-        except:
-            verboseprint('exception while parsing TLS handshake record')
-            return
-        client = '{0}:{1}'.format(socket.inet_ntoa(ip.src), ip.data.sport)
-        server = '{0}:{1}'.format(socket.inet_ntoa(ip.dst), ip.data.dport)
-        if isinstance(handshake.data, dpkt.ssl.TLSClientHello):  # 1
-            print('--> ClientHello {0} -> {1}'.format(client, server))
-            parse_client_hello(handshake)
-            break
-        if isinstance(handshake.data, dpkt.ssl.TLSServerHello):  # 2
-            print('<-- ServerHello {1} <- {0}'.format(client, server))
-            parse_server_hello(handshake.data)
-            break
-        if (handshake.type == 11):
-            print('<-- Certificate {0} <- {1}'.format(client, server))
-            break
-        if (handshake.type == 12):
-            print('<-- ServerKeyExchange {1} <- {0}'.format(server, client))
-            break
-        if (handshake.type == 13):
-            print('<-- CertificateRequest {1} <- {0}'.format(client, server))
-            break
-        if (handshake.type == 14):
-            print('<-- ServerHelloDone {1} <- {0}'.format(client, server))
-            break
-        if (handshake.type == 15):
-            print('--> CertificateVerify {0} -> {1}'.format(client, server))
-            break
-        if (handshake.type == 16):
-            print('[+++] --> ClientKeyExchange {0} -> {1}'.format(client, server))
-            break
-        if (handshake.type == 20):
-            print('--> Finished {0} -> {1}'.format(client, server))
-            break
-        print('[-] Unrecognized handshake type: {0}'.format(hexlify(handshake.data.data)))
+        record_type = pretty_print_name('tls_record', record.type)
+        verboseprint('captured TLS record type {0}'.format(record_type))
+        if record_type == 'handshake':
+            parse_tls_handshake(ip, record.data)
+        if record_type == 'alert':
+            print('[+] TLS Alert message')
+            verboseprint(hexlify(stream))
+        if record_type == 'change_ciper':
+            print('[+] Change cipher message')
         sys.stdout.flush()
 
 
-def number_of_bytes(number):
-    return int(log(number, 256) + 1)
+def parse_tls_handshake(ip, data):
+    """
+    Parses TLS Handshake message contained in data according to their type.
+    """
+    try:
+        handshake_type = ord(data[:1])
+        verboseprint('First 10 bytes {0}'.
+                     format(hexlify(data[:10])))
+        if handshake_type == 4:
+            print('[#] New Session Ticket is unfortunately not implemented yet')
+            return
+        else:
+            handshake = dpkt.ssl.TLSHandshake(data)
+    except dpkt.ssl.SSL3Exception as exception:
+        verboseprint('exception while parsing TLS handshake record: {0}'.
+                     format(exception))
+        return
+    except dpkt.dpkt.NeedData as exception:
+        verboseprint('exception while parsing TLS handshake record: {0}'.
+                     format(exception))
+        return
+    client = '{0}:{1}'.format(socket.inet_ntoa(ip.src), ip.data.sport)
+    server = '{0}:{1}'.format(socket.inet_ntoa(ip.dst), ip.data.dport)
+    if handshake.type == 0:
+        print('<-- Hello Request {0} <- {1}'.format(client, server))
+    if handshake.type == 1:
+        print('--> ClientHello {0} -> {1}'.format(client, server))
+        parse_client_hello(handshake)
+    if handshake.type == 2:
+        print('<-- ServerHello {1} <- {0}'.format(client, server))
+        parse_server_hello(handshake.data)
+    if handshake.type == 4:
+        print('NEW SESSION TICKET MESSAGE TYPE')
+    if handshake.type == 11:
+        print('<-- Certificate {0} <- {1}'.format(client, server))
+    if handshake.type == 12:
+        print('<-- ServerKeyExchange {1} <- {0}'.format(server, client))
+    if handshake.type == 13:
+        print('<-- CertificateRequest {1} <- {0}'.format(client, server))
+    if handshake.type == 14:
+        print('<-- ServerHelloDone {1} <- {0}'.format(client, server))
+    if handshake.type == 15:
+        print('--> CertificateVerify {0} -> {1}'.format(client, server))
+    if handshake.type == 16:
+        print('[+++] --> ClientKeyExchange {0} -> {1}'.format(client, server))
+    if handshake.type == 20:
+        print('--> Finished {0} -> {1}'.format(client, server))
 
 
 def unpacker(type_string, packet):
@@ -232,7 +262,6 @@ def parse_client_hello(handshake):
     compressions = []
     cipher_suites = []
     extensions = []
-#    handshake_length = number_of_bytes(len(hello))
     payload = handshake.data.data
     session_id, payload = unpacker('p', payload)
     cipher_suites, pretty_cipher_suites = parse_extension(payload, 'cipher_suites')
@@ -253,14 +282,15 @@ def parse_client_hello(handshake):
 
 
 def parse_extensions(payload):
+    """
+    Parse data as one or more TLS extensions.
+    """
     extensions = []
     if (len(payload) <= 0):
         return
-
     print('[*]   Extensions:')
     extensions_len, payload = unpacker('H', payload)
     verboseprint('Extensions Length: {0}'.format(extensions_len))
-
     while (len(payload) > 0):
         extension = Extension(payload)
         extensions.append(extension)
@@ -271,13 +301,17 @@ def parse_extensions(payload):
 
 def parse_extension(payload, type_name):
     """
-    Parses the extension within the payload based on the type_name
-    Returns an array of raw values as well as an array of prettified values
+    Parses an extension based on the type_name.
+    Returns an array of raw values as well as an array of prettified values.
     """
     entries = []
     pretty_entries = []
     format_list_length = 'H'
+    format_entry = 'B'
     list_length = 0
+    if type_name == 'elliptic_curves':
+        format_list_length = 'H'
+        format_entry = 'H'
     if type_name == 'ec_point_formats':
         format_list_length = 'B'
     if type_name == 'compression_methods':
@@ -285,7 +319,8 @@ def parse_extension(payload, type_name):
         format_entry = 'B'
     if len(payload) > 1:  # contents are a list
         list_length, payload = unpacker(format_list_length, payload)
-        verboseprint('type {0}, list length is {1}'.format(type_name, list_length))
+        verboseprint('type {0}, list type is {1}, number of entries is {2}'.
+                     format(type_name, format_list_length, list_length))
     if type_name == 'status_request':
         _type, payload = unpacker('B', payload)
         format_entry = 'H'
@@ -293,7 +328,6 @@ def parse_extension(payload, type_name):
         return payload, hexlify(payload)
     if type_name == 'SessionTicket_TLS':
         return payload, hexlify(payload)
-    format_entry = 'B'
     if type_name == 'cipher_suites':
         format_entry = 'H'
     if type_name == 'supported_groups':
@@ -315,7 +349,10 @@ def parse_extension(payload, type_name):
             pretty_entries.append('{0}-{1}'.format(pretty_print_name('signature_algorithms_hash', entry >> 8),
                                                     pretty_print_name('signature_algorithms_signature', entry % 256)))
         else:
-            pretty_entries.append(pretty_print_name(type_name, entry))
+            if format_entry.lower() == 'p':
+                pretty_entries.append(entry)
+            else:
+                pretty_entries.append(pretty_print_name(type_name, entry))
     return entries, pretty_entries
 
 
@@ -324,6 +361,10 @@ def pretty_print_name(name_type, name_value):
     if name_type in PRETTY_NAMES:
         if name_value in PRETTY_NAMES[name_type]:
             name_value = PRETTY_NAMES[name_type][name_value]
+        else:
+            name_value = '{0}: unknown value {1}'.format(name_value, name_type)
+    else:
+            name_value = 'unknown type: {0}'.format(name_type)
     return name_value
 
 
@@ -351,16 +392,17 @@ def start_listening(interface, cap_filter):
     """
     Starts the listening process with an optional filter.
     """
- #   try:
-    pc = pcap.pcap(name=interface)
-    pc.setfilter(cap_filter)
+    try:
+        capture = pcap.pcap(name=interface)
+        capture.setfilter(cap_filter)
+    except OSError as exception:
+        print('[-] Issue: {0}'.format(exception))
+        sys.exit(-1)
     while True:
-        print('[+] listening on {0}'.format(pc.name))
+        print('[+] listening on {0}'.format(capture.name))
         sys.stdout.flush()
-        pc.loop(0, analyze_packet)
+        capture.loop(0, analyze_packet)
         print('[-] stopping')
-#    except:
-        print('[-] issue while opening interface')
 
 
 if __name__ == "__main__":
